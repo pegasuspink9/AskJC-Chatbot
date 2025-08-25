e5dab7bcb623de418d4f897a6903cdb40c36dbdc
Leon Dev <pegasuspink9@gmail.com>
2025-08-25 20:50:30 +0800

    with Backend

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -8,7 +8,6 @@ import {
   StyleSheet,
   Dimensions
 } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { getColors, Spacing } from '../constants/theme';
 import { useTheme } from '../constants/ThemeContext';
